6e6e348d828452b2b060325fe678b993cda81215
Chandra Mohan S <chandramohan131198@gmail.com>
2024-07-29 18:25:02 +0530

    created modal

--- force-app/main/default/lwc/createNewCashBookRecordModal/createNewCashBookRecordModal.js
+++ force-app/main/default/lwc/createNewCashBookRecordModal/createNewCashBookRecordModal.js
@@ -1,8 +1,12 @@
 import { LightningElement, api, track } from 'lwc';
+import createNewCashBook from '@salesforce/apex/CashBookController.createNewCashBook';
 export default class CreateNewCashBookRecordModal extends LightningElement    
 {
     @track isOpen = false;
-
+    @track cashBookName;
+    @track disableSave = true;
+    @track showError = false;
+    @track inputClass = '';
     @api
     openModal() 
     {
@@ -16,7 +20,34 @@ export default class CreateNewCashBookRecordModal extends LightningElement
 
     handleSave() 
     {
-        console.log('Save-->')
-        this.isOpen = false;
+        createNewCashBook({ Name: this.cashBookName})
+            .then(result => 
+                {
+                    this.isOpen = false;
+                })
+            .catch(error => 
+                {
+                    console.error('Error creating record: ', error);
+                });
+        
+    }
+    handleChange(event)
+    {
+        if(event.target.label == 'Cash Book Name')
+        {
+            this.cashBookName = event.target.value;
+            if(this.cashBookName.trim()==='')
+            {
+                this.disableSave=true;
+                this.showError = true; 
+                this.inputClass = 'slds-has-error';
+            }
+            else
+            {
+                this.disableSave=false;
+                this.showError = false;
+                this.inputClass = '';
+            }
+        }
     }
 }
